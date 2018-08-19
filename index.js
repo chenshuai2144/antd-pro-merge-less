9b3430134820a0c2f8a8416254b4a6e419142bfa
陈帅 <qixian.cs@outlook.com>
2018-08-19 11:26:29 +0800

    优化算法，增加编译速度

--- index.js
+++ index.js
@@ -4,49 +4,42 @@
  * 由于没有开源插件，所以自己撸了一个
  */
 
-const fs = require('fs');
-const path = require('path');
-const getLocalIdentName = require('./getLocalIdentName');
-const AddlocalIdentName = require('./AddlocalIdentName');
-const replacedefaultLess = require('./replacedefaultLess');
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const getLocalIdentName = require("./getLocalIdentName");
+const AddlocalIdentName = require("./AddlocalIdentName");
+const replacedefaultLess = require("./replacedefaultLess");
 // read less file list
-const lessArray = ['@import "../node_modules/antd/lib/style/themes/default.less";'];
-
+const lessArray = [
+  '@import "../node_modules/antd/lib/style/themes/default.less";'
+];
 const loopAllLess = parents => {
-  const paths = fs.readdirSync(parents);
   const promiseList = [];
-  paths.forEach(itemPath => {
-    if (itemPath === 'style' || itemPath === 'demo') {
-      return;
-    }
-    // file status
-    const fileStatus = fs.lstatSync(path.join(parents, itemPath));
-    // is file
-    // is Directory
-    if (fileStatus.isDirectory()) {
-      return loopAllLess(path.join(parents, itemPath));
-    }
-    // is less file
-    if (itemPath.indexOf('.less') > -1) {
-      const relaPath = path.join(parents, itemPath);
+  glob
+    .sync(parents + "/**/**.less", { ignore: "**/node_modules/**" })
+    .forEach(relaPath => {
       // post css add localIdentNameplugin
       const fileContent = replacedefaultLess(relaPath);
       // push less file
       promiseList.push(
-        AddlocalIdentName(relaPath, fileContent, getLocalIdentName(relaPath)).then(result => {
+        AddlocalIdentName(
+          relaPath,
+          fileContent,
+          getLocalIdentName(relaPath)
+        ).then(result => {
           lessArray.push(result);
         })
       );
-    }
-  });
+    });
   return Promise.all(promiseList);
 };
 
 class mergeLessPlugin {
   constructor(options) {
     const defaulOptions = {
-      stylesDir: path.join(__dirname, './src/'),
-      outFile: path.join(__dirname, './tmp/ant.design.pro.less'),
+      stylesDir: path.join(__dirname, "./src/"),
+      outFile: path.join(__dirname, "./tmp/ant.design.pro.less")
     };
     this.options = Object.assign(defaulOptions, options);
     this.generated = false;
@@ -54,7 +47,7 @@ class mergeLessPlugin {
 
   apply(compiler) {
     const { options } = this;
-    compiler.plugin('emit', (compilation, callback) => {
+    compiler.plugin("emit", (compilation, callback) => {
       const { outFile } = options;
       // covert less
       if (fs.existsSync(outFile)) {
@@ -63,7 +56,7 @@ class mergeLessPlugin {
         fs.mkdirSync(path.dirname(outFile));
       }
       loopAllLess(options.stylesDir).then(() => {
-        fs.writeFileSync(outFile, lessArray.join('\n'));
+        fs.writeFileSync(outFile, lessArray.join("\n"));
         callback();
       });
     });
